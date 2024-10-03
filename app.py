3c09f4d5f6404d2f5a72aafba744357e6102b772
inceluser <filkrsjnov2000@gmail.com>
2024-10-03 18:26:29 +0300

    Update app.py

--- app.py
+++ app.py
@@ -91,6 +91,8 @@ def download(video_id):
         'writethumbnail': True,
         'embedthumbnail': True,
         'addmetadata': True,
+        # Добавляем прокси
+        'proxy': '212.127.93.185:8081',  # Указываем прокси-сервер
     }
 
     try:
